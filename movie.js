60c5664cf36b35e46509bf5f4fa0aa30c13137ab
yena <jntantmsemt@naver.com>
2023-06-02 11:00:03 +0900

    마지막

--- movie.js
+++ movie.js
@@ -1,12 +1,67 @@
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODQyMDliZTk2ODliN2JjNzI2MDBjNWM0OTljZTZkMyIsInN1YiI6IjY0NzA4OTVjMTNhMzIwMDExNmI2OTVkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AV-O0oDiPcF0xAmY70nzPy5uiWwiH8jJ4jiy-8OCFFc'
-  }
-};
+const API_KEY = 'api_key=284209be9689b7bc72600c5c499ce6d3';
+const BASE_URL = "https://api.themoviedb.org/3/";
+const API_URL = BASE_URL+'/movie/popular?language=en-US&'+API_KEY;
+const IMG_URL = 'http://image.tmdb.org/t/p/w500';
+const searchURL= BASE_URL + '/search/movie?'+API_KEY;
 
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+const main =document.getElementById('main');
+const form =document.getElementById('form');
+const search=document.getElementById('search');
+
+
+getMovies(API_URL);
+
+function getMovies(url) {
+  fetch(url).then(res => res.json()).then(data => {
+      console.log(data.results)
+      showMovies(data.results);
+      
+    
+  })
+}
+
+function showMovies(data) {
+  main.innerHTML='';
+  data.forEach(movie => {
+    const{title, poster_path, vote_average, overview, id}= movie;
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie-card');
+    movieEl.innerHTML= `
+
+          <a href="#"><img src="${IMG_URL+poster_path}" alt="${title}" onclick="alert('영화 id: ${id}')"></a>
+          <div class="view">
+            <h3 class="card-title">${title}</h3>
+            <p class="${getColor(vote_average)}">${vote_average.toFixed(1)}</p>
+            <p class="overview">${overview}</p>
+          </div>
+    `
+
+    main.appendChild(movieEl)
+  })
+}
+
+ let getColor= (vote)=> {
+  if(vote>= 8){
+    return 'green'
+  }else if(vote>= 5){
+    return "orange"
+  }else{
+    return "red"
+  }
+}
+
+
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if(searchTerm) {
+    getMovies(searchURL+'&query='+searchTerm)
+  }else{
+    getMovies(API_URL)
+  }
+})
+
+
